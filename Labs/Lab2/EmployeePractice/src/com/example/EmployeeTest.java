cdbfc04a1177f542db1f448268d421b1e9b08e08
Bekzat <b_tursyn@kbtu.kz>
2023-03-02 19:23:36 +0600

    first commit

--- Labs/Lab2/EmployeePractice/src/com/example/EmployeeTest.java
+++ Labs/Lab2/EmployeePractice/src/com/example/EmployeeTest.java
@@ -5,11 +5,11 @@ public class EmployeeTest {
 
     public static void main(String[] args) {
         Employee user = new Employee();
-        Employee uesr2 = new Employee(3)
-        user.setEmpId(101);
+        Employee uesr2 = new Employee(3);
+        // user.setEmpId(101);
         user.setName("Jane Smith");
-        user.setSsn ("012-34-5678");
-        user.setSalary(120_345.27);
+        // user.setSsn ("012-34-5678");
+        // user.setSalary(120_345.27);
 
         System.out.println ("Employee id:        " + user.getEmpId());
         System.out.println ("Employee name:      " + user.getName());
