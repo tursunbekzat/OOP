4f2e1d8e820477edbad29649e8f05e2eaba5172e
Bekzat <b_tursyn@kbtu.kz>
2023-03-30 11:21:32 +0600

    Lab7.3

--- Labs/Lab7/Generics-partial/Generics-Practice03/src/com/example/generics/SortShirtByCount.java
+++ Labs/Lab7/Generics-partial/Generics-Practice03/src/com/example/generics/SortShirtByCount.java
@@ -5,8 +5,9 @@ import java.util.Comparator;
 public class SortShirtByCount implements Comparator<Shirt> {
 
     public int compare(Shirt s1, Shirt s2) {
-        // Your code here
-        return 0;
+        Long c1 = Long.valueOf(s1.getCount());
+        Long c2 = Long.valueOf(s2.getCount());  
+        return c1.compareTo(c2);    
 
     }
 }
